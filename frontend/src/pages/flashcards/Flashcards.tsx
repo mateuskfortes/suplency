87511649e47e2e3bc0d8fb1e55dae07d5d4e8a25
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2024-08-29 17:00:03 -0300

    iniciei os flashcards

--- frontend/src/pages/flashcards/Flashcards.tsx
+++ frontend/src/pages/flashcards/Flashcards.tsx
@@ -1,8 +1,63 @@
+import { useEffect, useRef, useState } from "react"
+import Flashcard from "../../components/Flashcard"
+
+const content = [
+	{
+		id: 1,
+		question: "Quem é viado?",
+		answer: "José",
+	},
+	{
+		id: 2,
+		question: "Quantos anus você tem?",
+		answer: "1 e ele é grande",
+	},
+	{
+		id: 3,
+		question: 'Nenê?',
+		answer: 'Buaaaaaaaaaaaaaa',
+	},
+	{
+		id: 4,
+		question: 'O que o arthur falou da professora?',
+		answer:'Vagabunda',
+	}
+]
+
 const Flashcards = () => {
+	const [currentFlashcardIndex, setCurrentFlashcardIndex] = useState(NaN)
+	const [isRunning, setIsRunning] = useState(false)
+
+	useEffect(() => {
+		if (isRunning) {
+		  setCurrentFlashcardIndex(0); 
+		}
+	  }, [isRunning]);
+
+	function toggleIsRunning() {
+		setIsRunning(!isRunning)
+	}
+
+	function nextFlashcard() {
+		setCurrentFlashcardIndex(prevIndex => {
+			const nextIndex = prevIndex + 1;
+			return nextIndex < content.length ? nextIndex : 0; 
+		  });
+	}
+
+	const fc = content[currentFlashcardIndex]
+
 	return (
-		<div>
-		aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-		</div>
+		<>
+			{ isRunning && fc &&
+				<div>
+					<Flashcard key={fc.id} question={fc.question} answer={fc.answer} />
+				
+					<button onClick={nextFlashcard}>next</button>
+				</div>
+			}
+			<button onClick={toggleIsRunning}>vai</button>
+		</>
 	)
 }
 
