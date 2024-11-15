ed5c698406a3cdfeab0d2f98af25723876a5eed3
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2024-11-15 08:07:55 -0300

    Adding django-cors-headers

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'client.apps.ClientConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,8 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = config('ALLOW_CORS', cast=bool, default=False)
+CORS_ALLOW_CREDENTIALS = config('ALLOW_CORS', cast=bool, default=False)
 
 ROOT_URLCONF = 'setup.urls'
 
