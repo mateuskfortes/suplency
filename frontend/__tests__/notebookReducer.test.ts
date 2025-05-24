f1fb1f41160a8b75b6da645fdf4c1bd0bc0694e9
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2025-05-24 13:09:05 -0300

    test(notebook): add test for adding a page

--- frontend/__tests__/notebookReducer.test.ts
+++ frontend/__tests__/notebookReducer.test.ts
@@ -1,44 +1,102 @@
-import React, { act, Children, useState } from "react";
+import { useState } from "react";
 import { renderHook } from "@testing-library/react";
-import useNotebook, { findObj } from "../src/assets/notebookReducer";
+import { findObj, notebookReducer } from "../src/assets/notebookReducer";
 import { describe, it, expect, vi } from "vitest";
 import { withHistory } from "slate-history";
 import { withReact } from "slate-react";
 import { createEditor } from "slate";
 import { emptyPage } from "../src/components/Notebook/Notebook";
-import { defaultContent } from "../src/pages/Study";
+import { v4 as uuid } from "uuid";
+
+
+const sbId = uuid()
+const pgId = uuid()
+const defaultContent = {
+    'last_subject': sbId,
+    'subject': [
+        {
+            'id': sbId,
+            'name': 'subject',
+            'color': 'red',
+            'last_page': pgId,
+            'page': [
+                {
+                    'id': pgId,
+                    'number': 0,
+                    'color': 'green',
+                    'content': [
+                        {
+                            type: 'paragraph', 
+                            children: [{ text: 'page 1' }],
+                        },
+                    ],
+                    'subject': sbId,
+                },
+                {
+                    'id': pgId,
+                    'number': 1,
+                    'color': 'red',
+                    'content': [
+                        {
+                            type: 'paragraph', 
+                            children: [{ text: 'page 2' }],
+                        },
+                    ],
+                    'subject': sbId,
+                }
+            ]
+        }
+    ]
+}
 
 describe("Notebook Reducer", () => {
-    const createNotebookReducer = () => {
-        const [ editor ] = renderHook(() => useState(() => withHistory(withReact(createEditor())))).result.current
-        const currentSubjectHandler = findObj(defaultContent.subject, defaultContent.last_subject);
-        const currentPageHandler = findObj(currentSubjectHandler.page, currentSubjectHandler.last_page);
-        const updateEditorContent = vi.fn((content: any = state.currentPage.content) => {
-                const point = { path: [0, 0], offset: 0 };
-                editor.selection = { anchor: point, focus: point };
-                editor.children = content || emptyPage;
-            });
-            
-        const initialState = {
-            editor,
-            content: defaultContent,
-            currentSubject: currentSubjectHandler,
-            currentPage: currentPageHandler,
-            updateEditorContent,
-        }
-
-        const [ state, dispatch ] = renderHook(() => useNotebook(initialState)).result.current
-
-        return {
-            state,
-            dispatch,
-            initialState,
-        }
+    const [ editor ] = renderHook(() => useState(() => withHistory(withReact(createEditor())))).result.current
+    const currentSubjectHandler = findObj(defaultContent.subject, defaultContent.last_subject);
+    const currentPageHandler = findObj(currentSubjectHandler.page, currentSubjectHandler.last_page);
+    const updateEditorContent = vi.fn((content: any = prevState.currentPage.content) => {
+            const point = { path: [0, 0], offset: 0 };
+            editor.selection = { anchor: point, focus: point };
+            editor.children = content || emptyPage;
+        });
+    const prevState = {
+        editor,
+        content: defaultContent,
+        currentSubject: currentSubjectHandler,
+        currentPage: currentPageHandler,
+        updateEditorContent,
     }
 
-    it("should initialize the state correctly", () => {
-        const { state, initialState } = createNotebookReducer()
+    it("Should add a page", () => {
+        const action = {
+            type: "ADD_PAGE",
+            payload: {
+                id: uuid(),
+                number: 1,
+                content: [
+                    {
+                        type: 'paragraph',
+                        children: [{ text: 'new page 2' }],
+                    },
+                ]
+            }
+        }
+        const finalState = notebookReducer(prevState, action)
 
-        expect(JSON.stringify(state)).toEqual(JSON.stringify(initialState))
+        // Check if the page was added
+        expect(finalState.currentSubject.page).toHaveLength(3)
+        expect(Object.is(finalState.currentSubject.page[1], action.payload)).toBeTruthy()
+
+        // Check if the new page is now the current page
+        expect(Object.is(finalState.currentPage, action.payload)).toBeTruthy()
+
+        // Check if the page 1 was not modified
+        expect(Object.is(finalState.currentSubject.page[0], finalState.currentSubject.page[0])).toBeTruthy()
+
+        // Check if the old page 2 is now page 3
+        const prevPg2 = prevState.currentSubject.page[1]
+        const nowPg3 = finalState.currentSubject.page[2]
+        expect(nowPg3.id).toEqual(prevPg2.id)
+        expect(nowPg3.number).toEqual(prevPg2.number + 1)
+        expect(Object.is(prevPg2.content, nowPg3.content)).toBeTruthy()
     })
 }) 
--- frontend/src/assets/notebookReducer.ts
+++ frontend/src/assets/notebookReducer.ts
@@ -14,115 +14,118 @@ const updateSubject = (state: any, newSubject: any) => {
     }
 }
 
-const useNotebook = (initializer: any) => {
-    const notebookReducer = (state: any, action: any) => {
 
-        const addPage = () => {
-            // Update page
-            const pages = state.currentSubject.page.map((pg: any) => ({
-                ...pg,
-                number: pg.number >= action.payload.number ? pg.number + 1 : pg.number,
-            }))
-            pages.splice(action.payload.number, 0, action.payload)
-            const updatedSb = {
-                ...findObj(state.content.subject, state.currentSubject.id),
-                page: pages
-            }
-            
-            const newState = updateSubject(state, updatedSb)
-            return changePage(newState, action.payload.id);
-        }
-    
-        const changePage = (st: any = state, id: string = action.payload) => {
-            st.currentPage.content = st.editor.children
-            findObj(st.content.subject, st.currentSubject.id).last_page = id
-            const currentPageHandler = findObj(st.currentSubject.page, id);
-            st.updateEditorContent(currentPageHandler.content) 
-            
-            return {
-                ...st,
-                currentPage: currentPageHandler
-            };
-        }
+export const notebookReducer = (state: any, action: any) => {
 
-        const deletePage = () => {
-            const pages = state.currentSubject.page.map((pg: any) => ({
-                ...pg,
-                number: pg.number > state.currentPage.number ? pg.number - 1 : pg.number,
-            })).filter((pg: any) => pg.id !== state.currentPage.id)
-
-            const updatedSb = {
-                ...state.currentSubject,
-                page: pages
-            }
-
-            const newState = updateSubject(state, updatedSb)
-            console.log('antes', state.currentSubject.page)
-            console.log('dps', pages)
-            const pgN = state.currentPage.number
-            return changePage(newState, pgN > 0 ? 
-                                        newState.currentSubject.page[pgN - 1].id : 
-                                        newState.currentSubject.page[0].id)
-        }
-    
-        const addSubject = () => {
-            const newState = {
-                ...state,
-                content: {
-                    ...state.content,
-                    subject: [...state.content.subject, action.payload]
-                }
-            }
-            return changeSubject(newState, action.payload.id)
-        }
-    
-        const changeSubject = (st: any = state, id: string = action.payload) => {
-            const currentSubjectHandler = findObj(st.content.subject, id)
-            const currentPageHandler = findObj(currentSubjectHandler.page, currentSubjectHandler.last_page)
-            st.updateEditorContent(currentPageHandler.content)
-            return {
-                ...st,
-                currentSubject: currentSubjectHandler,
-                currentPage: currentPageHandler
-            };
+    // Inserts a new page into the current subject
+    const addPage = (st: any, payload: any) => {
+        // Update page
+        const pages = st.currentSubject.page.map((pg: any) => ({
+            ...pg,
+            number: pg.number >= payload.number ? pg.number + 1 : pg.number,
+        }))
+        pages.splice(payload.number, 0, payload)
+        const updatedSb = {
+            ...findObj(st.content.subject, st.currentSubject.id),
+            page: pages
         }
         
-        const deleteSubject = () => {
-            const newState = {
-                ...state,
-                content: {
-                    ...state.content,
-                    subject: state.content.subject.filter((sb: any) => sb.id !== action.payload)
-                }
-            }
-            return changeSubject(newState, newState.content.subject[0].id)
+        const newState = updateSubject(st, updatedSb)
+        return changePage(newState, payload.id);
+    }
+
+    const changePage = (st: any = state, id: string = action.payload) => {
+        st.currentPage.content = st.editor.children
+        findObj(st.content.subject, st.currentSubject.id).last_page = id
+        const currentPageHandler = findObj(st.currentSubject.page, id);
+        st.updateEditorContent(currentPageHandler.content) 
+        
+        return {
+            ...st,
+            currentPage: currentPageHandler
+        };
+    }
+
+    const deletePage = () => {
+        const pages = state.currentSubject.page.map((pg: any) => ({
+            ...pg,
+            number: pg.number > state.currentPage.number ? pg.number - 1 : pg.number,
+        })).filter((pg: any) => pg.id !== state.currentPage.id)
+
+        const updatedSb = {
+            ...state.currentSubject,
+            page: pages
         }
 
-        const setContent = () => {
-            const currentSubjectHandler = findObj(action.payload.subject, action.payload.last_subject)
-            const currentPageHandler = findObj(currentSubjectHandler.page, currentSubjectHandler.last_page)
-            state.updateEditorContent(currentPageHandler.content)
-            return {
-                ...state,
-                content: action.payload,
-                currentSubject: currentSubjectHandler,
-                currentPage: currentPageHandler,
-            };
+        const newState = updateSubject(state, updatedSb)
+        console.log('antes', state.currentSubject.page)
+        console.log('dps', pages)
+        const pgN = state.currentPage.number
+        return changePage(newState, pgN > 0 ? 
+                                    newState.currentSubject.page[pgN - 1].id : 
+                                    newState.currentSubject.page[0].id)
+    }
+
+    const addSubject = () => {
+        const newState = {
+            ...state,
+            content: {
+                ...state.content,
+                subject: [...state.content.subject, action.payload]
+            }
+        }
+        return changeSubject(newState, action.payload.id)
+    }
+
+    const changeSubject = (st: any = state, id: string = action.payload) => {
+        const currentSubjectHandler = findObj(st.content.subject, id)
+        const currentPageHandler = findObj(currentSubjectHandler.page, currentSubjectHandler.last_page)
+        st.updateEditorContent(currentPageHandler.content)
+        return {
+            ...st,
+            currentSubject: currentSubjectHandler,
+            currentPage: currentPageHandler
+        };
+    }
     
+    const deleteSubject = () => {
+        const newState = {
+            ...state,
+            content: {
+                ...state.content,
+                subject: state.content.subject.filter((sb: any) => sb.id !== action.payload)
+            }
         }
-     
-        switch (action.type) {
-            case "SET_CONTENT": return setContent()
-            case "CHANGE_SUBJECT":return changeSubject()
-            case "CHANGE_PAGE": return changePage()
-            case "ADD_SUBJECT": return addSubject()
-            case "ADD_PAGE": return addPage();
-            case "DELETE_SUBJECT": return deleteSubject()
-            case "DELETE_PAGE": return deletePage()
-            default:
-                return state;
-        }
-    };
+        return changeSubject(newState, newState.content.subject[0].id)
+    }
+
+    const setContent = () => {
+        const currentSubjectHandler = findObj(action.payload.subject, action.payload.last_subject)
+        const currentPageHandler = findObj(currentSubjectHandler.page, currentSubjectHandler.last_page)
+        state.updateEditorContent(currentPageHandler.content)
+        return {
+            ...state,
+            content: action.payload,
+            currentSubject: currentSubjectHandler,
+            currentPage: currentPageHandler,
+        };
+
+    }
+ 
+    switch (action.type) {
+        case "SET_CONTENT": return setContent()
+        case "CHANGE_SUBJECT":return changeSubject()
+        case "CHANGE_PAGE": return changePage()
+        case "ADD_SUBJECT": return addSubject()
+        case "ADD_PAGE": return addPage(state, action.payload);
+        case "DELETE_SUBJECT": return deleteSubject()
+        case "DELETE_PAGE": return deletePage()
+        default:
+            return state;
+    }
+};
+
+const useNotebook = (initializer: any) => {
     return useReducer(notebookReducer, initializer)
 }
 
